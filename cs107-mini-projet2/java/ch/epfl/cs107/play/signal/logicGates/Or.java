e15c3256f1ba74f9bfda572e57bfc026a57c4cd3
Julian <Julian@DESKTOP-958N43G.gare38>
2018-12-10 21:49:29 +0100

    or javadoc

--- cs107-mini-projet2/java/ch/epfl/cs107/play/signal/logicGates/Or.java
+++ cs107-mini-projet2/java/ch/epfl/cs107/play/signal/logicGates/Or.java
@@ -8,14 +8,28 @@ package ch.epfl.cs107.play.signal.logicGates;
 import ch.epfl.cs107.play.signal.logic.Logic;
 import ch.epfl.cs107.play.signal.logic.LogicSignal;
 
+/**
+ * Class modelizing the "OR" logic gate for two signals
+ * @author Julian Blackwell
+ * @author Aman Bansal
+ *
+ */
 public class Or extends LogicSignal {
 
+	//Two Logic to be processed
 	private Logic one, two;
 	
+	/**
+	 * Default constructor
+	 * @param one (Logic) : first logic element in the OR function
+	 * @param two (Logic) : second logic element in the OR function
+	 */
 	public Or(Logic one, Logic two) {
 		this.one = one;
 		this.two = two;
 	}
+	
+	//Extends LogicSignal
 	
 	@Override
 	public boolean isOn() {
