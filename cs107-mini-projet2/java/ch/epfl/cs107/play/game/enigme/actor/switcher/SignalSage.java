f0ea668528ae25093127cd48c0f09d1ad78efdfb
Julian <Julian@128.179.146.105>
2018-12-10 12:35:36 +0100

    signal f'n sages

---
+++ cs107-mini-projet2/java/ch/epfl/cs107/play/game/enigme/actor/switcher/SignalSage.java
@@ -0,0 +1,32 @@
+/*
+ *	Author:		Julian Blackwell
+ *	Date:		10 Dec 2018
+ */
+
+package ch.epfl.cs107.play.game.enigme.actor.switcher;
+
+import ch.epfl.cs107.play.game.areagame.Area;
+import ch.epfl.cs107.play.game.areagame.actor.Orientation;
+import ch.epfl.cs107.play.game.enigme.actor.Sage;
+import ch.epfl.cs107.play.math.DiscreteCoordinates;
+import ch.epfl.cs107.play.signal.logic.Logic;
+
+public class SignalSage extends Sage implements Logic {
+
+	private Orientation correctOrientation;
+	
+	public SignalSage(Area area, Orientation orientation, DiscreteCoordinates position, String wisdom, Orientation correctOrientation) {
+		super(area, orientation, position, wisdom);
+		this.correctOrientation = correctOrientation;
+	}
+
+	@Override
+	public boolean isOn() {
+		if(getOrientation().equals(correctOrientation)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+}
