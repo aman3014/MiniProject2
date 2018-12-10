c363c7f6c8f1d1505e349fb221c2fc3fffcdd00a
amanb <amanb@DESKTOP-ODLI3IU>
2018-12-10 23:05:29 +0100

    s

--- cs107-mini-projet2/java/ch/epfl/cs107/play/game/enigme/area/levels/Enigme0.java
+++ cs107-mini-projet2/java/ch/epfl/cs107/play/game/enigme/area/levels/Enigme0.java
@@ -11,7 +11,7 @@ import ch.epfl.cs107.play.game.enigme.actor.SignalRock;
 import ch.epfl.cs107.play.game.enigme.actor.collectable.Egg;
 import ch.epfl.cs107.play.game.enigme.actor.collectable.Key;
 import ch.epfl.cs107.play.game.enigme.actor.switcher.Bonfire;
-import ch.epfl.cs107.play.game.enigme.actor.SageSignal;
+import ch.epfl.cs107.play.game.enigme.actor.SignalSage;
 import ch.epfl.cs107.play.game.enigme.area.EnigmeArea;
 import ch.epfl.cs107.play.io.FileSystem;
 import ch.epfl.cs107.play.math.DiscreteCoordinates;
@@ -44,10 +44,10 @@ private Actor sageNorth, sageEast, sageSouth, sageWest;
 		
 		sageIntro = new Sage(this, Orientation.UP, new DiscreteCoordinates(10, 24), "The key to the door is found on a far away land. And Remember! Helping others on your way is the key to getting there !");
 		
-		sageNorth = new SageSignal(this, Orientation.LEFT, new DiscreteCoordinates(6, 14), "I seek the star that guided the pirates...", Orientation.UP);
-		sageEast = new SageSignal(this, Orientation.DOWN, new DiscreteCoordinates(4, 16), "I seek a glimpse of dawn...", Orientation.RIGHT);
-		sageSouth = new SageSignal(this, Orientation.UP, new DiscreteCoordinates(6, 18), "Help me face the warmth of family...", Orientation.DOWN);
-		sageWest = new SageSignal(this, Orientation.RIGHT, new DiscreteCoordinates(8, 16), "I seek the crimson eye before twilight...", Orientation.LEFT);
+		sageNorth = new SignalSage(this, Orientation.LEFT, new DiscreteCoordinates(6, 14), "I seek the star that guided the pirates...", Orientation.UP);
+		sageEast = new SignalSage(this, Orientation.DOWN, new DiscreteCoordinates(4, 16), "I seek a glimpse of dawn...", Orientation.RIGHT);
+		sageSouth = new SignalSage(this, Orientation.UP, new DiscreteCoordinates(6, 18), "Help me face the warmth of family...", Orientation.DOWN);
+		sageWest = new SignalSage(this, Orientation.RIGHT, new DiscreteCoordinates(8, 16), "I seek the crimson eye before twilight...", Orientation.LEFT);
 		bonfire = new Bonfire(this, Orientation.DOWN, new DiscreteCoordinates(6, 16), false, new MultipleAnd((Logic)sageNorth,(Logic)sageEast, (Logic)sageSouth, (Logic)sageWest));
 		
 		sageExit = new Sage(this, Orientation.LEFT, new DiscreteCoordinates(29, 1), "I have heard of a hidden path amongst the trees ! After all, who doesn't want to return home ?");
