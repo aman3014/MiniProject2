53f1e195e48e76146081d1c5c67bc9498ea926de
Julian <Julian@128.179.146.105>
2018-12-10 12:40:31 +0100

    EGG

---
+++ cs107-mini-projet2/java/ch/epfl/cs107/play/game/enigme/actor/collectable/Egg.java
@@ -0,0 +1,32 @@
+/*
+ *	Author:		Julian Blackwell
+ *	Date:		10 Dec 2018
+ */
+
+package ch.epfl.cs107.play.game.enigme.actor.collectable;
+
+import ch.epfl.cs107.play.game.areagame.Area;
+import ch.epfl.cs107.play.game.areagame.actor.Orientation;
+import ch.epfl.cs107.play.game.areagame.actor.Sprite;
+import ch.epfl.cs107.play.game.enigme.actor.Collectable;
+import ch.epfl.cs107.play.math.DiscreteCoordinates;
+import ch.epfl.cs107.play.math.Vector;
+import ch.epfl.cs107.play.window.Canvas;
+
+public class Egg extends Collectable {
+
+	private Sprite image;
+	
+	public Egg(Area area, Orientation orientation, DiscreteCoordinates position) {
+		super(area, orientation, position);
+		this.image = new Sprite("egg.1", 0.75f, 0.75f, this, null, new Vector(0.125f, 0.125f));
+	}
+
+	@Override
+	public void draw(Canvas canvas) {
+		if(!isCollected()) {
+			image.draw(canvas);
+		}	
+	}
+
+}
