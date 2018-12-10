f5a0994bdf9f8e0931a297ea3481204534faa309
Julian <Julian@DESKTOP-958N43G.gare38>
2018-12-10 23:37:11 +0100

    level1

--- cs107-mini-projet2/java/ch/epfl/cs107/play/game/enigme/area/levels/Level1.java
+++ cs107-mini-projet2/java/ch/epfl/cs107/play/game/enigme/area/levels/Level1.java
@@ -8,7 +8,6 @@ package ch.epfl.cs107.play.game.enigme.area.levels;
 import java.util.List;
 
 import ch.epfl.cs107.play.game.actor.Actor;
-import ch.epfl.cs107.play.game.areagame.actor.Orientation;
 import ch.epfl.cs107.play.game.enigme.actor.SignalDoor;
 import ch.epfl.cs107.play.game.enigme.area.EnigmeArea;
 import ch.epfl.cs107.play.io.FileSystem;
@@ -16,8 +15,14 @@ import ch.epfl.cs107.play.math.DiscreteCoordinates;
 import ch.epfl.cs107.play.signal.logic.Logic;
 import ch.epfl.cs107.play.window.Window;
 
+/**
+ * Level 1 "Room/Level" (instance of EnigmeArea)
+ * @author Julian Blackwell, Aman Bansal
+ *
+ */
 public class Level1 extends EnigmeArea {
 	
+	//actor in area
 	private Actor doorToLevelSelector;
 	
 	@Override
@@ -25,6 +30,7 @@ public class Level1 extends EnigmeArea {
 		return "Level1";
 	}
 	
+	@Override
 	public boolean begin(Window window, FileSystem fileSystem) {
 		DiscreteCoordinates position = new DiscreteCoordinates(5, 0);
 		doorToLevelSelector = new SignalDoor(Logic.TRUE, this, "LevelSelector", new DiscreteCoordinates(1, 6), position, position);
